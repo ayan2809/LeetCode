8c43b0a6425d478c6949ecc49bcf6ecb8506cc96
Ayan Sadhukhan <42286904+ayan2809@users.noreply.github.com>
2021-08-08 22:41:04 +0530

    Time: 68 ms (67.41%), Space: 10.6 MB (71.51%) - LeetHub

---
+++ coin-change/coin-change.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount==0)
+            return 0;
+        int len=coins.size();
+        int dp[len+1][amount+1];
+        int mini=-1;
+        //memset(dp, 0, sizeof(dp[0][0])*(len+1)*(amount+1));
+        for(int i=0;i<=len;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                // if(i==0 && j==0)
+                // {
+                //     dp[i][j]=INT_MAX-1;
+                //     continue;
+                // }
+                 if(j==0){
+                    dp[i][j]=0;continue;}
+                if(i==0){
+                    dp[i][j]=INT_MAX-1;continue;}
+                // if(i==1){
+                //     if(j%coins[i-1]==0)
+                //     {
+                //         dp[i][j]=(int)j/coins[i-1];
+                //     }
+                //     else
+                //     {
+                //         dp[i][j]=INT_MAX;
+                //     }
+                //     continue;
+                // }
+                
+        
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]=min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+                    
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        if(dp[len][amount]==INT_MAX-1)
+            return -1;
+        return dp[len][amount];
+    }
+};
