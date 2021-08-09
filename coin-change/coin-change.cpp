b66858c80ed9e83fe7667627b4d37fe1295c7294
Ayan Sadhukhan <42286904+ayan2809@users.noreply.github.com>
2021-08-09 12:36:18 +0530

    Time: 68 ms (67.04%), Space: 10.6 MB (77.90%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -6,33 +6,15 @@ public:
         int len=coins.size();
         int dp[len+1][amount+1];
         int mini=-1;
-        //memset(dp, 0, sizeof(dp[0][0])*(len+1)*(amount+1));
+        
         for(int i=0;i<=len;i++)
         {
             for(int j=0;j<=amount;j++)
             {
-                // if(i==0 && j==0)
-                // {
-                //     dp[i][j]=INT_MAX-1;
-                //     continue;
-                // }
-                 if(j==0){
+                if(j==0){
                     dp[i][j]=0;continue;}
                 if(i==0){
                     dp[i][j]=INT_MAX-1;continue;}
-                if(i==1){
-                    if(j%coins[i-1]==0)
-                    {
-                        dp[i][j]=(int)j/coins[i-1];
-                    }
-                    else
-                    {
-                        dp[i][j]=INT_MAX-1;
-                    }
-                    continue;
-                }
-                
-        
                 if(coins[i-1]<=j)
                 {
                     dp[i][j]=min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
