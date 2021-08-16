004833301856b283c60db23e2c97e5ae3bec7e57
Ayan Sadhukhan <42286904+ayan2809@users.noreply.github.com>
2021-08-16 08:59:14 +0530

    Time: 16 ms (8.45%), Space: 11.4 MB (5.67%) - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -6,7 +6,6 @@ public:
         {
             umap[it]+=1;    
         }
-        //sort(arr.begin(),arr.end());
         for(auto it: arr)
         {
             cout<<umap[it]<<" ";
@@ -14,8 +13,6 @@ public:
             {
                 if(umap[it]>1)
                     return true;
-                //else if(umap[it]==1)
-                    //return false;
                 continue;
             }
             long long check=it*2;
