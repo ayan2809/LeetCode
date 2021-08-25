198d254fa5e5e6cf16ce2333fb28848bc2c83573
Ayan Sadhukhan <42286904+ayan2809@users.noreply.github.com>
2021-08-25 22:54:39 +0530

    Time: 48 ms (73.06%), Space: 28.3 MB (45.17%) - LeetHub

---
+++ array-partition-i/array-partition-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans=0;
+        for(int i=0;i<nums.size();i+=2)
+        {
+            ans+=min(nums[i],nums[i+1]);
+        }
+        return ans;
+    }
+};
