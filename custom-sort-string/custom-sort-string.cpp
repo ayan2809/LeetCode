0d35220a42dfb41b4f21577f1b479a7276e789a8
Ayan Sadhukhan <42286904+ayan2809@users.noreply.github.com>
2021-09-24 23:58:11 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.88%) - LeetHub

---
+++ custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+         sort(s.begin(), s.end(),
+             [&](char a, char b) 
+              { return order.find(a) < order.find(b); });
+        return s;
+    }
+};
