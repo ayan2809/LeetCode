a75196452ca3572e5d3f5042f6aae05254be5c9b
Ayan <42286904+ayan2809@users.noreply.github.com>
2022-01-07 21:22:38 +0530

    Time: 210 ms (5.02%), Space: 59.1 MB (37.40%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        vector<int> left;
-        vector<int> right;
-        int maxil=-1;
-        int maxir=-1,n=height.size()-1;
-        int out=0;
-        for(int i=0;i<height.size();i++)
-        {
-            maxil=max(height[i], maxil);
-            left.push_back(maxil);
-            maxir=max(height[n-i], maxir);
-            right.push_back(maxir);
-        }
-        // int k=height.size()-1,ans=0;
-        // int i=0,x=k;
-        // for(int i=0;i<height.size();i++){
-        //     cout<<left[i]<<" "<<right[i]<<endl;
-        // }
-        // while(k>0)
-        // {
-        //     ans=max(ans, min(left[i],right[i])*x);
-        //     i++;
-        //     k--;
-        //     x-=2;
-        // }
-        
+    int maxArea(vector<int>& height) {        
         int i=0,j=height.size()-1, ans=0;
         while(i<j)
         {
@@ -33,25 +8,11 @@ public:
             ans=max(ans,(j-i)*x);
             if(x==height[i])
             {
-                // if(height[i]<height[i+1])
-                // {
-                    i++;
-                // }
-                // else
-                // {
-                //     j--;
-                // }
+                    i++;                
             }
             else if(x==height[j])
             {
-                // if(height[j]<height[j-1])
-                // {
                     j--;
-                // }
-                // else
-                // {
-                //     i++;
-                // }
             }
             else
             {
