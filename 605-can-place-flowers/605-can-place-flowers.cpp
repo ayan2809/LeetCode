b820167e6668991e407b7399e3888abaca298d3e
Ayan <42286904+ayan2809@users.noreply.github.com>
2022-01-18 15:44:35 +0530

    Time: 37 ms (9.49%), Space: 20.3 MB (69.55%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,84 +1,41 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        vector<int> left;
-        vector<int> right;
-        int maxil=-1;
-        int maxir=INT_MAX-1,m=flowerbed.size()-1;
-        int out=0;
-        for(int i=0;i<flowerbed.size();i++)
-        {
-            if(flowerbed[i]==1)
-            maxil=max(i, maxil);
-            left.push_back(maxil);
-            if(flowerbed[m-i]==1)
-            maxir=min((m-i), maxir);
-            right.push_back(maxir);
-        }
-        reverse(right.begin(), right.end());
-        for(int i=0;i<=m;i++)
-        {
-            if((abs(left[i]-i)>=2 )&& (abs(i-right[i])>=2 ))
+        int siz=flowerbed.size();
+        int count=0;
+        int i=2;
+        if(siz==1 && flowerbed[0]==0)
+            count++;
+        else
             {
-                n--;
-                int x=left[i];
-                int j=i;
-                while(left[j]==x && j<m)
-                {
-                    if(left[j]!=x)
-                        break;
-                    left[j++]=i;
-                }
-                left[j]=i;
-            }
-            else if((left[i]==-1 ) && (abs(i-right[i])>=2 ))
-            {
-                n--;
-                int x=left[i];
-                int j=i;
-                while(left[j]==x && j<m)
-                {
-                    if(left[j]!=x)
-                        break;
-                    left[j++]=i;
-                }
-                left[j]=i;
-            }
-            else if((abs(left[i]-i)>=2 ) && (right[i]==INT_MAX-1 ))
-            {
-                n--;
-                int x=left[i];
-                int j=i;
-                while(left[j]==x && j<m)
-                {
-                    if(left[j]!=x)
-                        break;
-                    left[j++]=i;
-                }
-                left[j]=i;
-            }
-            else if(left[i]==-1 && right[i]==INT_MAX-1)
-            {
-                n--;
-                int x=left[i];
-                int j=i;
-                while(left[j]==x && j<m)
-                {
-                    if(left[j]!=x)
-                        break;
-                    left[j++]=i;
-                }
-                left[j]=i;
-            }
-        }
-        cout<<endl;
-        for(auto it: left)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
-        if(n<=0)
-        return true;
-        return false;
+                if(flowerbed[0]==0 && flowerbed[1]==0)
+                       count++;
+                
+                  
+                    while(i<siz)
+                       {
+                           if(i==siz-1)
+                              {
+                                 if(flowerbed[siz-1]==0 && flowerbed[siz-2]==0)
+                                   count++; 
+                               break;
+                              }
+                           else
+                              {
+                                 if(flowerbed[i-1]==0 && flowerbed[i]==0 && flowerbed[i+1]==0)
+                                 {
+                                    count++;
+                                    i=i+2;
+                                 }
+                                  else
+                                    i++;
+                              }  
+}
+            }                      
+           // cout<<count;
+        
+        return (count>=n);
     }
+      //  cout<<count<<endl;
+    // }
 };
