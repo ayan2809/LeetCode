267f9c5b2f8b28d0399a665efb8e5d55fc14c1d3
Ayan <42286904+ayan2809@users.noreply.github.com>
2022-07-12 11:01:07 +0530

    Time: 3 ms (39.22%), Space: 5.9 MB (72.68%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        return num1+num2;
+        if(num2==0)
+            return num1;
+        if(num2>0)
+            return sum(num1+1, num2-1);\
+        // else if(num2<0)
+        return sum(num1-1,num2+1);
     }
 };
