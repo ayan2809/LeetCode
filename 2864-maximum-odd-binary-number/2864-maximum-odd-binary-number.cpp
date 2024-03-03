a2d4cea956a8ed82a868a544ebca4dedcb08f305
Ayan <42286904+ayan2809@users.noreply.github.com>
2024-03-03 16:21:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (18.52%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -3,18 +3,6 @@ public:
     string maximumOddBinaryNumber(string s) {
         int one=count(s.begin(), s.end(), '1');
         int n = s.length();
-        string ans(n, '0');
-        for(int i=0;i<n;i++)
-        {
-            if(one>1)
-            {
-                s[i]='1';
-                one--;
-            }
-            else
-                s[i]='0';
-        }
-        s[n-1]='1';
-        return s;
+        return string(one-1, '1')+string(n-one, '0')+string(1, '1');
     }
 };
